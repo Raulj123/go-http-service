a286dc32308f59ac264932aa6aa0376f6a71fb28
Raulj123 <jarquinr121@gmail.com>
2025-03-20 22:44:08 -0700

    add post employee route, encoder/decoder

--- server/employees.go
+++ server/employees.go
@@ -1,7 +1,7 @@
 package server
 
 import (
-	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -10,13 +10,18 @@ import (
 )
 
 // TODO context shi and queries blah blah oh and loogers?
-
+// Employee represents an employee with minimal information
+type Employee struct {
+	Id int `json:"id"`
+    Name string `json:"name"`
+	Manager string `json:"manager"`
+	StartDate string `json:"start_date"`
+}
 // This function handles getEmployees route in the future should handle all employee related routes
 func (s *Server) getEmployees() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request){
-		ctx := context.Background()
 		queries := sqlite.New(s.DB)
-		employees, err := queries.GetAllEmployees(ctx)
+		employees, err := queries.GetAllEmployees(r.Context())
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
@@ -30,13 +35,12 @@ func (s *Server) getEmployees() http.HandlerFunc {
 
 func (s *Server) getStartSoon() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request){
-		ctx := context.Background()
 		queries := sqlite.New(s.DB)
 		parmas := sqlite.GetEmployeesStartingSoonParams{
 			StartDate: utils.GetCurrentDate(),
 			StartDate_2: utils.GetFutureDate(),
 		}
-		employees, err := queries.GetEmployeesStartingSoon(ctx, parmas)
+		employees, err := queries.GetEmployeesStartingSoon(r.Context(), parmas)
 		
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -51,4 +55,61 @@ func (s *Server) getStartSoon() http.HandlerFunc {
 		fmt.Fprintf(w, "No Employees starting soon")
 	}
 	}
+}
+
+func (s *Server) postEmployee() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request){
+		if r.Method != http.MethodPost {
+			http.Error(w, "Invalid method",http.StatusBadRequest)
+			return
+		}
+		
+		d,err := decodeJson[Employee](r)
+		
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		queries := sqlite.New(s.DB)
+		params := sqlite.InsertEmployeeParams{
+			ID: int64(d.Id),
+			Name: d.Name,
+			Manager: d.Manager,
+			StartDate: d.StartDate,
+		}
+		employee, err := queries.InsertEmployee(r.Context(), params)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		fmt.Println("Employee: ", employee)
+		// does go infer the type here?
+		encodeJson(w,http.StatusOK, d)
+	}
+}
+
+// My brain has a hard time getting these two right
+// Decode → "Deconstruct JSON" → "JSON to Go".
+//Encode → "Construct JSON" → "Go to JSON".
+
+// This generic function takes any type and decodes json to specifed type
+func decodeJson[T any](r *http.Request) (T, error){
+	var v T
+	d := json.NewDecoder(r.Body)
+	// no unkown fields bitch
+	d.DisallowUnknownFields()
+	if err := d.Decode((&v)); err != nil {
+		return v, fmt.Errorf("decode json %w", err)
+	}
+	return v, nil
+}
+
+func encodeJson[T any](w http.ResponseWriter, staus int, v T) error {
+	w.Header().Set("content/type", "application/json")
+	w.WriteHeader(staus)
+	if err := json.NewEncoder(w).Encode(v); err != nil {
+		return fmt.Errorf("encode json %w", err)
+	}
+	fmt.Fprintf(w,"Recored! %v", v)
+	return nil
 }
--- server/routes.go
+++ server/routes.go
@@ -3,5 +3,6 @@ package server
 func (s *Server) routes(){
 	// TODO: add cors,subrouter,auth in future
 	s.Router.Get("/employees", s.getEmployees())
-	s.Router.Get("/start", s.getStartSoon())
+	s.Router.Get("/employees/starting-soon", s.getStartSoon())
+	s.Router.Post("/employees/add", s.postEmployee())
 }
