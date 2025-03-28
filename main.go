a8d8a667e88bfa688619c9366504b7e5a9946d0e
Raulj123 <jarquinr121@gmail.com>
2025-03-27 18:23:37 -0700

    add: shutdown

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"context"
 	"database/sql"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/Raulj123/go-service/config"
-	"github.com/Raulj123/go-service/employee"
+	"github.com/Raulj123/go-service/models/employee"
 	"github.com/go-chi/chi"
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -32,9 +38,29 @@ func initDB() *sql.DB{
 // TODO: add global logger
 func main(){
 	db := initDB()
+	defer db.Close()
+
 	prov := employee.NewEmpProvider(db)
+	s := &http.Server{
+		Addr: ":3000",
+	}
+	go func() {
+		if err := s.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("HTTP server error: %v", err)
+		}
+		log.Println("no new connections")
+	}()
+
+	sigC := make(chan os.Signal, 1)
+	signal.Notify(sigC, syscall.SIGINT, syscall.SIGTERM)
+	<-sigC
+	shutdownCtx, shutdownRelease := context.WithTimeout(context.Background(), 10*time.Second)
+    defer shutdownRelease()
+
+    if err := s.Shutdown(shutdownCtx); err != nil {
+        log.Fatalf("HTTP shutdown error: %v", err)
+    }
+    log.Println("Graceful shutdown complete.")
 	r := chi.NewRouter()
 	r.Mount("/", employee.NewHandler(prov))
-	defer db.Close()
-	log.Fatal("cannot serve:", http.ListenAndServe(":8080", r))
 }
